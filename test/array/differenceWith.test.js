652b704cef2cc45e28a5fd619c313add29cbf01e
Hamza Hejja <hamzahejja@hotmail.com>
2020-12-12 12:13:34 +0200

    unit-test for _.differenceWith with a custom defined comparator function

--- test/array/differenceWith.test.js
+++ test/array/differenceWith.test.js
@@ -1,5 +1,6 @@
 const _differenceWith = require('lodash/differenceWith');
 const { differenceWith } = require('../../functions/array');
+const { difference } = require('lodash');
 
 describe('_.differenceWith(array, [values], [comparator])', () => {
   it('It Should Match Result with _.differenceWith with Equality Comparator', () => {
@@ -15,5 +16,27 @@ describe('_.differenceWith(array, [values], [comparator])', () => {
 
     expect(_differenceWith(objects, [{ 'x': 1, 'y': 2 }], comparator))
       .toEqual(differenceWith(objects, [{ 'x': 1, 'y': 2 }], comparator));
+  });
+
+  it ('It Should Match Result with _.differenceWith with a Custom Defined Comparator', () => {
+    const students = [
+      { gpa: 3.85, isGradute: true },
+      { gpa: 3.5, isGraduate: false },
+      { gpa: 3.7, isGradute: true },
+      { gpa: 4.0, isGraduate: false}
+    ];
+
+    const excludedValues = [
+      { gpa: 4.0, isGraduate: true },
+      { gpa: 3.85, isGraduate: false}
+    ];
+
+    const comparator = (student, otherStudent) => {
+      return student['isGraduate'] === otherStudent['isGraduate'] &&
+        otherStudent['gpa'] > student['gpa'];
+    };
+
+    expect(_differenceWith(students, excludedValues, comparator))
+      .toEqual(differenceWith(students, excludedValues, comparator));
   });
 });
